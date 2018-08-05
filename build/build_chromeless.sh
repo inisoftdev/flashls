b9a979689f131a6318cc9fdbf19c6c5c0be6349a
KS Jun <ksjun@mkhq.co.kr>
2018-08-05 21:01:32 +0900

    Modified some compiler options.

--- build/build_chromeless.sh
+++ build/build_chromeless.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 OPT_DEBUG="-use-network=false \
-    -optimize=true \
-    -debug=true \
+    -compiler.debug \
     -library-path+=../lib/blooddy_crypto.swc \
     -define=CONFIG::LOGGING,true \
     -define=CONFIG::FLASH_25_0,true"
 
 OPT_RELEASE="-use-network=false \
     -optimize=true \
-    -debug=false \
     -library-path+=../lib/blooddy_crypto.swc \
     -define=CONFIG::LOGGING,false \
     -define=CONFIG::FLASH_25_0,true"
@@ -20,8 +18,9 @@ echo "Compiling bin/release/flashlsChromeless.swf"
     -o ../bin/release/flashlsChromeless.swf \
     $OPT_RELEASE \
     -target-player="25.0" \
-    -default-size 480 270 \
-    -default-background-color=0x000000
+    -default-size 640 360 \
+    -default-background-color=0x000000 \
+    -warnings
 #./add-opt-in.py ../bin/release/flashlsChromeless.swf
 
 echo "Compiling bin/debug/flashlsChromeless.swf"
@@ -30,6 +29,7 @@ echo "Compiling bin/debug/flashlsChromeless.swf"
     -o ../bin/debug/flashlsChromeless.swf \
     $OPT_DEBUG \
     -target-player="25.0" \
-    -default-size 480 270 \
-    -default-background-color=0x000000
+    -default-size 640 360 \
+    -default-background-color=0x000000 \
+    -warnings
 #./add-opt-in.py ../bin/debug/flashlsChromeless.swf
